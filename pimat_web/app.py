9eae715c6b705ef7d185e19eb77670d2af19317b
jorge.soares <jorge.soares@accolade.com>
2017-07-28 21:37:44 +0200

    mysql tests

--- pimat_web/app.py
+++ pimat_web/app.py
@@ -46,6 +46,22 @@ def index():
     cursor.execute("SELECT timestamp, temperature1, humidity, light1 from sensors where source='pimat_server'")
     data = cursor.fetchall()
     print(data)
+    timestamp= list()
+    temperature1 = list()
+    humidity = list()
+    light1 = list()
+
+    for row in data:
+        timestamp.append(row[0])
+        temperature1.append(row[1])
+        humidity.append(row[2])
+        light1.append(row[3])
+
+    print('lists:')
+    print(timestamp)
+    print(temperature1)
+    print(humidity)
+    print(light1)
 
     return render_template('index.html', pins=relay_pins, status=relay_status)
 
