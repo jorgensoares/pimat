40f2137cdc60ad734a43401a212ac59874550e5e
jorge.soares <jorge.soares@accolade.com>
2017-07-28 16:56:43 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': [
-            'pimat_server = pimat-server.__main__:main',
-            'pimat_web = pimat-web.app:main'
+            'pimat-server = pimat_server.__main__:main',
+            'pimat-web = pimat_web.app:main'
         ]
     },
     install_requires=['flask', 'configparser', 'python-crontab', 'RPi.GPIO', 'adafruit_python_dht'],
