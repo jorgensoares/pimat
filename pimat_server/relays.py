3e4a64c11eb96b8340566d62684181fc3a47e7d1
jorge.soares <jorge.soares@accolade.com>
2017-07-28 19:21:42 +0200

    remove previous commit

--- pimat_server/relays.py
+++ pimat_server/relays.py
@@ -27,7 +27,6 @@ class Relays(object):
 
     def start(self):
         call(['gpio', 'write', self.pin, '0'])
-        call(['gpio', 'mode', self.pin, 'out'])
         print(self.relay)
         relay_config.set('status', self.relay, '1')
 
@@ -36,7 +35,6 @@ class Relays(object):
 
     def stop(self):
         call(['gpio', 'write', self.pin, '1'])
-        call(['gpio', 'mode', self.pin, 'out'])
         print(self.relay)
         relay_config.set('status', self.relay, '0')
 
