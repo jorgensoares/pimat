6bac5f381e313014ebfbe9675b935f1eb98b0a34
jorge.soares <jorge.soares@accolade.com>
2017-07-28 20:57:23 +0200

    add sql script

---
+++ pimat_web/sql/pimat.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sensors (
+         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         timestamp DATETIME,
+         temperature1 FLOAT,
+         temperature2 FLOAT,
+         humidity FLOAT,
+         light1 FLOAT,
+         pressure FLOAT,
+         altitude FLOAT,
+         source VARCHAR(100)
+       );
+
