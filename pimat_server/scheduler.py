16505eaf0a6f03c040d882eebda956ae550a51a1
jorge.soares <jorge.soares@accolade.com>
2017-07-29 12:23:55 +0200

    bugfix cron job not executing

--- pimat_server/scheduler.py
+++ pimat_server/scheduler.py
@@ -6,13 +6,13 @@ cron = CronTab(user='root')
 
 def add_schedule(relay, start_time, stop_time):
     start_hour, start_minute = start_time.split(':')
-    start_job = cron.new(command='/usr/bin/python /opt/pimat/pimat_server/relays.py start %s' % relay,
+    start_job = cron.new(command='PATH=$PATH:/usr/local/bin /usr/local/bin/pimat-relay start %s' % relay,
                          comment='%s start' % relay)
     start_job.minute.on(start_minute)
     start_job.hour.on(start_hour)
     start_job.enable()
 
-    stop_job = cron.new(command='/usr/bin/python /opt/pimat/pimat_server/relays.py stop %s' % relay,
+    stop_job = cron.new(command='PATH=$PATH:/usr/local/bin /usr/local/bin/pimat-relay stop %s' % relay,
                         comment='%s stop' % relay)
     stop_hour, stop_minute = stop_time.split(':')
     stop_job.minute.on(stop_minute)
