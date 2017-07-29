866ed2f83254e1de7b7f763db3dfc8842c086a15
jorge.soares <jorge.soares@accolade.com>
2017-07-30 00:59:36 +0200

    version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='pimat',
     description='Raspberry Pi Multi Automation tool',
-    version='0.4.7',
+    version='0.5.0',
     url='https://github.com/jorgensoares/pimat',
     author='Jorge Soares',
     packages=find_packages(exclude=['tests']),
@@ -23,8 +23,7 @@ setup(
                       'pymysql',
                       'flask_sqlalchemy',
                       'MySQL-python',
-                      'flask-login',
-                      'bcrypt'],
+                      'flask-login'],
 
     package_data={'pimat_web': ['templates/*']}
 )
