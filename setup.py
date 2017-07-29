84f9ba3514115b1596b3391b19994dba6d822b15
jorge.soares <jorge.soares@accolade.com>
2017-07-29 20:44:30 +0200

    version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='pimat',
     description='Raspberry Pi Multi Automation tool',
-    version='0.4.6',
+    version='0.4.7',
     url='https://github.com/jorgensoares/pimat',
     author='Jorge Soares',
     packages=find_packages(exclude=['tests']),
