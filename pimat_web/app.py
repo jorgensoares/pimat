bd16554afd24651d175dc6b56986bceef9c25b89
jorge.soares <jorge.soares@accolade.com>
2017-08-11 16:27:35 +0200

    patch

--- pimat_web/app.py
+++ pimat_web/app.py
@@ -440,7 +440,6 @@ def profile():
 @login_required
 def monitoring():
     ip = pimat_config['pimat']['server_ip']
-    print(ip)
     return render_template('monitoring.html', ip=ip, version=version)
 
 
