8301d5c0ff9a4116fc4afb5ea598752747313f0b
jorge.soares <jorge.soares@accolade.com>
2017-08-12 12:13:24 +0200

    patch

--- pimat_web/app.py
+++ pimat_web/app.py
@@ -37,8 +37,8 @@ app.config['MAIL_SERVER']='mail.ocloud.cz'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'teste@ocloud.cz'
 app.config['MAIL_PASSWORD'] = 'zaq12wsx'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = 'teste@ocloud.cz'
 
 app.logger.addHandler(file_handler)
