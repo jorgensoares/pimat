68fbb8f8a94cf44246a4950215fdd32543d9cc51
jorge.soares <jorge.soares@accolade.com>
2017-08-13 16:08:26 +0200

    patch

---
+++ pimat_web/api.py
@@ -0,0 +1,59 @@
+from flask_restful import Resource, reqparse, marshal, fields
+from pimat_web.app import db, Sensors, Schedules, RelayLogger
+
+schedules_fields = {
+    'start_time': fields.String,
+    'stop_time': fields.String,
+    'relay': fields.String,
+    'id': fields.String
+}
+
+
+class SensorsAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('timestamp', type=str, required=True, location='json')
+        self.reqparse.add_argument('temperature1', type=float, default="", location='json')
+        self.reqparse.add_argument('temperature2', type=float, default="", location='json')
+        self.reqparse.add_argument('humidity', type=float, default="", location='json')
+        self.reqparse.add_argument('light1', type=float, default="", location='json')
+        self.reqparse.add_argument('pressure', type=float, default="", location='json')
+        self.reqparse.add_argument('altitude', type=float, default="", location='json')
+        self.reqparse.add_argument('source', type=str, required=True, location='json')
+        super(SensorsAPI, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        reading = Sensors(args['timestamp'], args['temperature1'], args['temperature2'], args['humidity'],
+                          args['light1'], args['pressure'], args['altitude'], args['source'])
+        db.session.add(reading)
+        db.session.commit()
+        return {'status': 'success'}, 201
+
+
+class SchedulesAPI(Resource):
+    def get(self):
+        schedules = Schedules.query.order_by(Schedules.relay.asc()).all()
+        return {'schedules': [marshal(schedule, schedules_fields) for schedule in schedules]}, 200
+
+
+class RelayLoggerAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('timestamp', type=str, required=True, location='json')
+        self.reqparse.add_argument('relay', type=str, required=True, default="", location='json')
+        self.reqparse.add_argument('pin', type=int, required=True, default="", location='json')
+        self.reqparse.add_argument('action', type=str, required=True, default="", location='json')
+        self.reqparse.add_argument('value', type=str, required=True, default="", location='json')
+        self.reqparse.add_argument('type', type=str, default="", location='json')
+        self.reqparse.add_argument('source', type=str, required=True, default="", location='json')
+        super(RelayLoggerAPI, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        action = RelayLogger(args['timestamp'], args['relay'], args['pin'], args['action'], args['value'], args['type'],
+                             args['source'])
+        db.session.add(action)
+        db.session.commit()
+
+        return {'status': 'success'}, 201
--- pimat_web/app.py
+++ pimat_web/app.py
@@ -17,14 +17,9 @@ import signal
 import sys
 import requests
 import json
-from flask_restful import Resource, reqparse, marshal, fields
-from wtforms import StringField, PasswordField, BooleanField, validators
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms.validators import DataRequired
-from werkzeug.utils import secure_filename
 import os
 from forms import LoginForm, PasswordForgotForm, PasswordResetForm, CreateUserForm, UpdateProfileForm
-
+from api import SensorsAPI, SchedulesAPI, RelayLoggerAPI
 
 version = __version__
 
@@ -86,63 +81,6 @@ def sigterm_handler(_signo, _stack_frame):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-schedules_fields = {
-    'start_time': fields.String,
-    'stop_time': fields.String,
-    'relay': fields.String,
-    'id': fields.String
-}
-
-
-class SensorsAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('timestamp', type=str, required=True, location='json')
-        self.reqparse.add_argument('temperature1', type=float, default="", location='json')
-        self.reqparse.add_argument('temperature2', type=float, default="", location='json')
-        self.reqparse.add_argument('humidity', type=float, default="", location='json')
-        self.reqparse.add_argument('light1', type=float, default="", location='json')
-        self.reqparse.add_argument('pressure', type=float, default="", location='json')
-        self.reqparse.add_argument('altitude', type=float, default="", location='json')
-        self.reqparse.add_argument('source', type=str, required=True, location='json')
-        super(SensorsAPI, self).__init__()
-
-    def post(self):
-        args = self.reqparse.parse_args()
-        reading = Sensors(args['timestamp'], args['temperature1'], args['temperature2'], args['humidity'],
-                          args['light1'], args['pressure'], args['altitude'], args['source'])
-        db.session.add(reading)
-        db.session.commit()
-        return {'status': 'success'}, 201
-
-
-class SchedulesAPI(Resource):
-    def get(self):
-        schedules = Schedules.query.order_by(Schedules.relay.asc()).all()
-        return {'schedules': [marshal(schedule, schedules_fields) for schedule in schedules]}, 200
-
-
-class RelayLoggerAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('timestamp', type=str, required=True, location='json')
-        self.reqparse.add_argument('relay', type=str, required=True, default="", location='json')
-        self.reqparse.add_argument('pin', type=int, required=True, default="", location='json')
-        self.reqparse.add_argument('action', type=str, required=True, default="", location='json')
-        self.reqparse.add_argument('value', type=str, required=True, default="", location='json')
-        self.reqparse.add_argument('type', type=str, default="", location='json')
-        self.reqparse.add_argument('source', type=str, required=True, default="", location='json')
-        super(RelayLoggerAPI, self).__init__()
-
-    def post(self):
-        args = self.reqparse.parse_args()
-        action = RelayLogger(args['timestamp'], args['relay'], args['pin'], args['action'], args['value'], args['type'],
-                             args['source'])
-        db.session.add(action)
-        db.session.commit()
-
-        return {'status': 'success'}, 201
 
 
 class User(db.Model):
