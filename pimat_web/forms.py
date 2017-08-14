a887120a5676f28b7f90165144d4a896345d03af
jorge.soares <jorge.soares@accolade.com>
2017-08-14 15:29:05 +0200

    revert commit

--- pimat_web/forms.py
+++ pimat_web/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField, validators
 from wtforms import StringField, PasswordField, validators
 from wtforms.validators import DataRequired
-from flask import current_app
 
 
 class LoginForm(FlaskForm):
@@ -12,8 +11,8 @@ class LoginForm(FlaskForm):
 class PasswordForgotForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
 
-    if current_app.config('RECAPTCHA') is True:
-        recaptcha = RecaptchaField('recaptcha')
+
+    recaptcha = RecaptchaField('recaptcha')
 
 
 class PasswordResetForm(FlaskForm):
@@ -25,8 +24,8 @@ class PasswordResetForm(FlaskForm):
     verify_new_password = PasswordField('verify_new_password')
     token = StringField('token', validators=[DataRequired()])
 
-    if current_app.config('RECAPTCHA') is True:
-        recaptcha = RecaptchaField('recaptcha')
+
+    recaptcha = RecaptchaField('recaptcha')
 
 
 class CreateUserForm(FlaskForm):
