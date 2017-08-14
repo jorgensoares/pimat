aa4c7473ccfa05db9c3c9d2edc4f8ee98af4924c
jorge.soares <jorge.soares@accolade.com>
2017-08-14 15:46:00 +0200

    patch

--- pimat_web/config.py
+++ pimat_web/config.py
@@ -8,17 +8,46 @@ LOG = config['pimat']['log']
 UPLOAD_FOLDER = config['pimat']['upload_folder']
 RELAY_CONFIG= config['pimat']['relay_config']
 SQLALCHEMY_DATABASE_URI = config['pimat']['sqlalchemy_database_uri']
-SQLALCHEMY_TRACK_MODIFICATIONS = config['pimat']['sqlalchemy_track_modifications']
-SQLALCHEMY_ECHO = config['pimat']['sqlalchemy_echo']
+
+if config['pimat']['sqlalchemy_track_modifications'] == 'True':
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+else:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+if config['pimat']['sqlalchemy_echo'] == 'True':
+    SQLALCHEMY_ECHO = True
+else:
+    SQLALCHEMY_ECHO = False
+
+
 MAIL_SERVER = config['pimat']['mail_server']
 MAIL_PORT = int(config['pimat']['mail_port'])
 MAIL_USERNAME = config['pimat']['mail_username']
 MAIL_PASSWORD = config['pimat']['mail_password']
-MAIL_USE_TLS = config['pimat']['mail_use_tls']
-MAIL_USE_SSL = config['pimat']['mail_use_ssl']
+
+if config['pimat']['mail_use_tls'] == 'True':
+    MAIL_USE_TLS = True
+else:
+    MAIL_USE_TLS = False
+
+if config['pimat']['mail_use_ssl'] == 'True':
+    MAIL_USE_SSL = True
+else:
+    MAIL_USE_SSL = False
+
 MAIL_DEFAULT_SENDER = config['pimat']['mail_default_sender']
-CSRF_ENABLED = config['pimat']['csrf_enabled']
-RECAPTCHA = config['pimat']['recaptcha']
+
+if config['pimat']['csrf_enabled'] == 'True':
+    CSRF_ENABLED = True
+else:
+    CSRF_ENABLED = False
+
+if config['pimat']['recaptcha'] == 'True':
+    RECAPTCHA = True
+else:
+    RECAPTCHA = False
+
+
 RECAPTCHA_PUBLIC_KEY = config['pimat']['recaptcha_public_key']
 RECAPTCHA_PRIVATE_KEY = config['pimat']['recaptcha_private_key']
 SECRET_KEY = config['pimat']['secret_key']
